8c2b14f584671872b74a6f5f7d9294807486f96b
GitAvi001 <avindusridharmawardhana@gmail.com>
2025-05-26 14:10:13 +0530

    React app, amplify and cognito configurations added

---
+++ my-cognito-quiz-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizapp99e809c5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
